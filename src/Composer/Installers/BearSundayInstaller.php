d9b8c95c11db3b577ee9d60eb7e89a316443e877
Richard McIntyre <richard.mackstar@gmail.com>
2013-10-22 21:09:16 +0100

    Removed inflectPackageVars for BEAR Sunday.

--- src/Composer/Installers/BearSundayInstaller.php
+++ src/Composer/Installers/BearSundayInstaller.php
@@ -4,17 +4,6 @@ namespace Composer\Installers;
 class BearSundayInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'app'     => 'apps/{$name}/'
+        'app' => 'apps/{$name}/'
     );
-
-    /**
-     * Format package name to CamelCase
-     */
-    public function inflectPackageVars($vars)
-    {
-        $vars['name'] = strtolower(str_replace(array('-', '_'), ' ', $vars['name']));
-        $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
-
-        return $vars;
-    }
 }
